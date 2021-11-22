fa9224e939fb699f01670146bf1463d60852a9a2
Danila.Manturov <danila.manturov@jetbrains.com>
2021-11-22 18:11:13 +0300

    fix dsl premerge step

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -60,7 +60,7 @@ object Build : BuildType({
                 echo %teamcity.pullRequest.source.branch%
             """.trimIndent()
         }
-        premergeRunner {
+        PreliminaryMerge {
             name = "premerge"
             //type = "premergeRunner"
 
